bdcb910e93d6357781fff030498aee28544530d3
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2021-08-02 17:12:28 -0300

    Estrutura inicial do primeiro ms

---
+++ camel-microservice/src/test/java/br/com/academy/camelmicroservice/CamelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.camelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
